853580b46ae883d2d08861c68e031469a07998a3
Pranav Modh <modhpranav21@gmail.com>
2023-05-04 16:01:44 -0400

    [UPDATE] hw13 with instance lock

---
+++ HW13-AccessCounterRelativePaths/src/edu/umb/cs681/hw13/AccessCounter.java
@@ -0,0 +1,67 @@
+package edu.umb.cs681.hw13;
+
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class AccessCounter {
+
+    private static volatile AccessCounter instance;
+
+    private static final ReentrantLock singletonInstanceLock = new ReentrantLock();
+    private final Map<Path, Integer> accessCountMap;
+    private final ReentrantReadWriteLock rwLock;
+
+
+    private AccessCounter() {
+        accessCountMap = new HashMap<>();
+        rwLock = new ReentrantReadWriteLock();
+    }
+
+    public static AccessCounter getInstance() {
+        if (instance == null) {
+            singletonInstanceLock.lock();
+            try {
+                if (instance == null) {
+                    instance = new AccessCounter();
+                }
+            } finally {
+                singletonInstanceLock.unlock();
+            }
+        }
+        return instance;
+    }
+
+
+    public void increment(Path path) {
+        rwLock.writeLock().lock();
+        try {
+            Integer count = accessCountMap.get(path);
+            if (count == null) {
+                count = 0;
+            }
+            accessCountMap.put(path, count + 1);
+        } finally {
+            rwLock.writeLock().unlock();
+        }
+    }
+
+    public int getCount(Path path) {
+        rwLock.readLock().lock();
+        try {
+            Integer count = accessCountMap.get(path);
+            if (count == null) {
+                count = 0;
+            }
+            return count;
+        } finally {
+            rwLock.readLock().unlock();
+        }
+    }
+
+    public Map<Path, Integer> getAccessCountMap() {
+        return accessCountMap;
+    }
+}
